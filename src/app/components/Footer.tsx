3fb81d2f9b27d4cd3f5d5d16569d8fba3fed272c
Temporal 76 <marc.pollet76@gmail.com>
2023-07-18 15:29:57 +0200

    edited footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className=' flex bg-black justify-around'>
-        <h1 className='text-sm md:text-base p-2'>Built with love by Marc Pollet</h1>
+        <h1 className='text-sm md:text-base p-2'>Built with Next JS by Marc Pollet</h1>
     </div>
   )
 }
